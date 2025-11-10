ea03714779769e29c879366feb5cde333da3c17f
mbs5 <templergreek@gmail.com>
2025-11-10 18:43:38 -0500

    Configure Vite for multi-page application routing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        application: resolve(__dirname, 'application.html'),
+        book: resolve(__dirname, 'book.html'),
+        thankyou: resolve(__dirname, 'thank-you.html')
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
